96e5cf6b0548b1ea52bcc17a423bc4b8ae65fafe
wmtong <wmtong@wustl.edu>
2016-02-29 19:33:12 -0600

    first commit

--- ParseInsta/AppDelegate.swift
+++ ParseInsta/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "InstagramWill"
+                configuration.clientKey = "wmtong"
+                configuration.server = "https://nameless-journey-97449.herokuapp.com/parse"
+
+            })
+        )
         return true
     }
 
---
+++ ParseInsta/LoginViewController.swift
@@ -0,0 +1,61 @@
+//
+//  LoginViewController.swift
+//  ParseInsta
+//
+//  Created by William Tong on 2/29/16.
+//  Copyright © 2016 William Tong. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+
+    @IBOutlet weak var passwordField: UITextField!
+
+    
+    
+    @IBAction func onSignIn(sender: AnyObject) {
+        PFUser.logInWithUsernameInBackground(usernameField.text!, password: passwordField.text!){(user: PFUser?, NSError)-> Void in
+            if user != nil {
+                print("you're logged in!")
+                
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+            }
+            
+        }
+    }
+    
+    @IBAction func onSignUp(sender: AnyObject) {
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+                // manually segue to logged in view
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
